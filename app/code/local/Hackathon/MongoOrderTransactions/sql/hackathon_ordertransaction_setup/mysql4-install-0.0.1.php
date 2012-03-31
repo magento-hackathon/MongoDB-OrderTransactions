85c530ca56377060092b483d7ed6dd530da76ccb
Vinai Kopp <vinai@netzarbeiter.com>
2012-03-31 22:10:04 +0200

    Add order ID sequence table

--- app/code/local/Hackathon/MongoOrderTransactions/sql/hackathon_ordertransaction_setup/mysql4-install-0.0.1.php
+++ app/code/local/Hackathon/MongoOrderTransactions/sql/hackathon_ordertransaction_setup/mysql4-install-0.0.1.php
@@ -15,7 +15,16 @@ $table->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, 10, array(
     'primary' => true,
     'unsigned' => true,
     'identity' => true,
-), 'Order ID Sequence Table');
+), 'Sequence Column')
+    ->addColumn('handle', Varien_Db_Ddl_Table::TYPE_INTEGER, 1, array(
+    'unsigned' => true,
+    'null' => false,
+), 'Key Column')
+->addIndex(
+    $installer->getConnection()->getIndexName($tableName, array('handle')),
+    array('handle'), array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE)
+)
+->setComment('Order ID Sequence Table');
 $installer->getConnection()->createTable($table);
 
 
@@ -28,6 +37,9 @@ if (! $lastId)
 {
     $lastId = 1;
 }
-$installer->run("ALTER TABLE {$tableName} AUTO_INCREMENT = {$lastId}");
+$installer->run("
+ALTER TABLE {$tableName} AUTO_INCREMENT = {$lastId};
+INSERT INTO {$tableName} (id, handle) VALUES ($lastId, 1);
+");
 
 $installer->endSetup();
