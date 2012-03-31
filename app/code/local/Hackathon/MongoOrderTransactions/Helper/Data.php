bc16f55a5830a4c906cab6e478189f219904a55b
Vinai Kopp <vinai@netzarbeiter.com>
2012-03-31 22:10:24 +0200

    Add order ID sequence generation method

--- app/code/local/Hackathon/MongoOrderTransactions/Helper/Data.php
+++ app/code/local/Hackathon/MongoOrderTransactions/Helper/Data.php
@@ -6,4 +6,14 @@ class Hackathon_MongoOrderTransactions_Helper_Data extends Mage_Core_Model_Abstr
     {
         return (string) Mage::getConfig()->getNode('global/hackathon_ordertransaction/mongo_db');
     }
+
+    public function getNewOrderIdFromSequence()
+    {
+        /* @var $connection Varien_Db_Adapter_Pdo_Mysql */
+        $resource = Mage::getSingleton('core/resource');
+        $connection = $resource->getConnection('default_read');
+        $table = $resource->getTableName('hackathon_ordertransaction/order_seq');
+        $connection->query("REPLACE INTO {$table} SET handle=1");
+        return $connection->lastInsertId($table);
+    }
 }
